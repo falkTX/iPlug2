dd9f30e377bb9b4205b1155adfce58d8ed8c529a
Niko Sound <contact@bluelab-plugs.com>
2021-01-29 00:22:53 +0100

    Linux compilation: BL-Gain12 compiled (app only). Several bugs. Need cmake improvements

---
+++ Scripts/cmake/WDL.cmake
@@ -0,0 +1,17 @@
+set(WDL_DIR ${IPLUG2_DIR}/WDL)
+
+#
+add_library(_wdl INTERFACE)
+set(WDL_SRC "${WDL_DIR}/")
+set(_wdl_src
+  besselfilter.h
+  fft.c
+  fft.h
+  resample.cpp
+  resample.h
+  )
+list(TRANSFORM _wdl_src PREPEND "${WDL_SRC}")
+iplug_target_add(_wdl INTERFACE
+  INCLUDE ${WDL_DIR}
+  SOURCE ${_wdl_src}
+)
